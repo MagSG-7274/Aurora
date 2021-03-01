3a4a8124a71a1a6552902a5c87f1f7d8ec4fb735
Magiccop990 <jtulek@at-rt.com>
2021-03-01 19:40:37 +0100

    :zap: Changes paths for api

--- src/main/java/com/stsf/aurora/controllers/BlacklistController.java
+++ src/main/java/com/stsf/aurora/controllers/BlacklistController.java
@@ -3,23 +3,20 @@ package com.stsf.aurora.controllers;
 
 import com.stsf.aurora.requestmodel.GenericResponse;
 import com.stsf.aurora.services.BlacklistService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("aurora/blocking")
+@RequestMapping("aurora/blacklist")
 public class BlacklistController {
 
     private final BlacklistService blacklistService = new BlacklistService();
 
-    @GetMapping("/black/add")
+    @PostMapping("/add")
     public GenericResponse<String> addBlacklist(@RequestBody String domain) {
         return blacklistService.addBlacklistService(domain);
     }
 
-    @GetMapping("/black/rem")
+    @PostMapping("/rem")
     public GenericResponse<String> removeBlackList(@RequestBody String domain) {
         return blacklistService.removeBlacklistService(domain);
     }
--- src/main/java/com/stsf/aurora/controllers/BlockingController.java
+++ src/main/java/com/stsf/aurora/controllers/BlockingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("aurora/adblock")
+@RequestMapping("aurora/blocking")
 public class BlockingController {
 
 
