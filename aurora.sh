6c4c32475d09ef16dcd7a4939af1272fe3de58f9
Magiccop990 <jtulek@at-rt.com>
2021-04-21 11:59:57 +0200

    :sparkles: Updates the installation script

--- aurora.sh
+++ aurora.sh
@@ -1,9 +1,28 @@
-sudo rm -rf /opt/Aurora
-cd /opt
-git clone https://github.com/MagSG-7274/Aurora.git
-cd /opt/Aurora
-sudo chmod +x gradlew
-./gradlew bootJar
+install() {
+  sudo rm -rf /opt/Aurora
+  cd /opt
+  git clone https://github.com/MagSG-7274/Aurora.git
+  touch config.json
+  cd /opt/Aurora
+  sudo chmod +x gradlew
+}
+
+run() {
+./gradlew clean bootJar
 cd /opt/Aurora/build/libs
 mv aurora* aurora.jar
 java -jar aurora.jar
+}
+
+##
+# MAIN BODY
+##
+
+
+if [ $1 == "-i" ];
+then
+  install
+elif [ $1 == "-r" ];
+then
+  run
+fi
