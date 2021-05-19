fdc4e997989bfd9c808b409d68e10924c0803008
MagSG-7274 <49731971+MagSG-7274@users.noreply.github.com>
2021-05-19 12:57:22 +0200

    :hammer: Returns semicolon to pass build

--- src/main/java/com/stsf/aurora/AuroraApplication.java
+++ src/main/java/com/stsf/aurora/AuroraApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuroraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuroraApplication.class, args)
+		SpringApplication.run(AuroraApplication.class, args);
 	}
 }
